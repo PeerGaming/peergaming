b067ceceae6687997a9ac18dd53c78d8fba2477f
Autarc <Autarc@gmail.com>
2013-01-29 00:12:06 +0100

    add extend to core

--- src/main.js
+++ src/main.js
@@ -22,13 +22,26 @@
 	var pg = {};
 
 
+	function extend ( target ) {
+
+		var source, key;
+
+		for ( var i = 1, length = arguments.length; i < length; i++ ) {
+
+			source = arguments[i];
+
+			for ( key in source ) if ( source.hasOwnProperty(key) ) target[key] = source[key];
+		}
+
+		return target;
+	}
 
 
-	//= require utils
-	//= require data
-	//= require network
-	//= require game
 
+	//= require "utils"
+	//= require "data"
+	//= require "network"
+	//= require "game"
 
 
 	return pg;
