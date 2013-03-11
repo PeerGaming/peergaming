3e3c39c70d634c14006139dfd798156494daa16c
Autarc <Autarc@gmail.com>
2013-03-11 02:56:32 +0100

    extend base

--- src/core/_base.js
+++ src/core/_base.js
@@ -1,15 +1,30 @@
 /**
- *	Base:
+ *	Base
+ *	====
  *
- *	Basic definitions
+ *	Basic wrapper definitions.
  */
 
 var pg = function(){
 
 };
 
-pg.VERSION = '0.1.1';
+pg.VERSION = '0.1.5';
 
+
+var ready = false;
+
+var instance;	// used for single reference !
+
+pg.queue = [];
+
+
+/**
+ *	Extends properties of an Object.
+ *
+ *	@param  {[type]} target [description]
+ *	@return {[type]}        [description]
+ */
 
 function extend ( target ) {
 
