feac9d78c5be82bd7a39135694c37020b680301b
Autarc <Autarc@gmail.com>
2013-05-16 01:51:50 +0200

    setup oAuth and data

---
+++ deps/server/js/scripts/auth.js
@@ -0,0 +1,16 @@
+/**
+ *  Authentication
+ *  ==============
+ *
+ *  Using OAuth credtinals to login to various services.
+ */
+
+// used for account handling
+
+
+// data.peergaming.net			// JSON output
+// auth.peergaming.net			// login request
+// demo.peergaming.net
+//
+
+
