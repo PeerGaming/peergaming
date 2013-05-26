5390155ec1c3087edce245dd0acefca960cee9f1
Autarc <Autarc@gmail.com>
2013-05-26 21:27:44 +0200

    setup media

--- src/meta/_media.js
+++ src/meta/_media.js
@@ -2,159 +2,108 @@
  *  Media
  *  =====
  *
- *	Handler just for mediastreaming (video & audio). Additional connection - which can be used
- *	besides the current handler/connection.
+ *  Handler just for mediastreaming (video & audio). Additional connection - which can be used
+ *  besides the current handler/connection.
+ *
+ *  // include _media in meta !
  */
 
+// handles types of connection: data connection or media connection (the firstis based on datachannel,
+// the other on mediastream - which can be used to broadcast e.g. video streams)
+// this.test = 1;
+// ICE wird vor dem 2.offer gesetzt und ist evlt in kompatible
+// 10 candiadtes - just after received offer - answer...  // second offer !
+
+// this.channels[ label ] = new Handler( channel, this.remote, label );
+// handler = new Handler( channel, this.remote, label );
+
+var Media = function ( local, remote, initiator, transport ) {
+
+  this.info = { local: local, remote: remote, pending: true };
+
+  if ( initiator ) this.info.initiator = true;
+  if ( transport ) this.info.transport = transport;
+
+  this.channels = {};
+
+  this.init();
+};
 
 
-// attachMediaStreams()
+Media.prototype.init = function(){
+
+  this.conn = new RTCPeerConnection( config.peerConfig, config.connectionConstraints );
+
+  this.checkStateChanges();
+
+  this.findICECandidates();
+
+  if ( this.info.initiator ) {
+
+    this.requestStream();
+  }
+};
 
 
-	// Connection.prototype.handleIncomingStreams = function(){
-
-	// 	var conn = this.conn;
-
-	// 	conn.onaddstream = function ( e ){
-
-	// 		console.log('[added stream]');
-	// 		console.log(e);
-			// var video = document.createElement('video');
-			// video.src = URL.createObjectURL( e.stream );
-			// video.autoplay = true;
-
-			// var box = document.createElement('div');
-			// box.textContent = this.remoteID;
-			// box.className = 'name';
-			// box.appendChild(video);
-
-			// document.body.appendChild( box );
-			//
+utils.inherit( Media, Connection );
 
 
-		// var remoteMediaStream=evt.stream;
-		            // if(remoteMedia==null){
-		            // remoteMedia=remoteMediaStream;
-		            // }
-		            // else
-		            // {
-		            //     remoteMedia.addTrack(remoteMediaStream.getVideoTracks()[0]); //add the video track to the existing stream
-		            // }
-		            // if(remoteVideo!=null)  {
-		            // //    $(remoteVideo).remove();
-		            //     //remoteVideo=null;
-		            // }
+Media.prototype.attachStream = function(){
 
-		// }.bind(this);
+  var conn = this.conn;
 
-		// conn.onremovestream = function ( e ) {
+  conn.onaddstream = function ( e ) {
 
-		// 	console.log('[removed stream]');
+    console.log('[added stream]');
+    console.log(e);
 
-			// document.getElementById('vid2').src = null;
-			// URL.revokeObjectURL( e.stream );
-		// };
+    var video = document.createElement('video');
+    video.src = URL.createObjectURL( e.stream );
+    video.autoplay = true;
+
+    var box = document.createElement('div');
+    box.textContent = this.remoteID;
+    box.className = 'name';
+    box.appendChild(video);
+
+    document.body.appendChild( box );
+  };
 
 
-		// window.test = function(){
+  conn.onremovestream = function ( e ) {
 
-			// device access
-			// var permissions = { audio: true, video: true };
+    console.log('[removed stream]');
 
-			// navigator.getUserMedia( permissions, function ( stream ) {
+    document.getElementById('vid2').src = null;
+    URL.revokeObjectURL( e.stream );
+  };
 
-
-			// 	var videoTracks = stream.getVideoTracks(),
-			// 		audioTracks = stream.getAudioTracks();
-
-			// 	// this.stream = stream;
-			// 	console.log(conn);
-			// 	conn.addStream( stream );
-
-				// var video = document.createElement('video');
-
-				// video.src = URL.createObjectURL(stream);
-				// video.autoplay = true;
-
-				// document.body.appendChild( video );
-
-	// 		}.bind(this));
-
-	// 	}.bind(this);
-	// };
+};
 
 
+Media.prototype.requestStream = function ( el ) {
 
+  // device access
+  var permissions = { audio: true, video: true };
 
+  win.navigator.getUserMedia( permissions, function ( stream ) {
 
+    var videoTracks = stream.getVideoTracks(),
+        audioTracks = stream.getAudioTracks();
 
-	// Connection.prototype.handleIncomingStreams = function(){
+    conn.addStream( stream );
 
-	// 	var conn = this.conn;
+    // this.createOffer();
 
-	// 	conn.onaddstream = function ( e ){
+    // local representation
+    if ( !el ) return;
 
-	// 		console.log('[added stream]');
-	// 		console.log(e);
-			// var video = document.createElement('video');
-			// video.src = URL.createObjectURL( e.stream );
-			// video.autoplay = true;
+    var video = document.createElement('video');
 
-			// var box = document.createElement('div');
-			// box.textContent = this.remoteID;
-			// box.className = 'name';
-			// box.appendChild(video);
+    video.src      = createObjectURL( stream );
+    video.autoplay = true;
 
-			// document.body.appendChild( box );
-			//
+    document.getElementById( el ).appendChild( video );
+  });
 
-
-		// var remoteMediaStream=evt.stream;
-		            // if(remoteMedia==null){
-		            // remoteMedia=remoteMediaStream;
-		            // }
-		            // else
-		            // {
-		            //     remoteMedia.addTrack(remoteMediaStream.getVideoTracks()[0]); //add the video track to the existing stream
-		            // }
-		            // if(remoteVideo!=null)  {
-		            // //    $(remoteVideo).remove();
-		            //     //remoteVideo=null;
-		            // }
-
-		// }.bind(this);
-
-		// conn.onremovestream = function ( e ) {
-
-		// 	console.log('[removed stream]');
-
-			// document.getElementById('vid2').src = null;
-			// URL.revokeObjectURL( e.stream );
-		// };
-
-
-		// window.test = function(){
-
-			// device access
-			// var permissions = { audio: true, video: true };
-
-			// navigator.getUserMedia( permissions, function ( stream ) {
-
-				// var videoTracks = stream.getVideoTracks(),
-					// audioTracks = stream.getAudioTracks();
-
-				// conn.addStream( stream );
-
-
-				// this.stream = stream;
-				// var video = document.createElement('video');
-
-				// video.src = URL.createObjectURL(stream);
-				// video.autoplay = true;
-
-				// document.body.appendChild( video );
-
-	// 		}.bind(this));
-
-	// 	}.bind(this);
-	// };
+};
