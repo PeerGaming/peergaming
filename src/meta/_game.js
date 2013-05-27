88d613ff1ed696b746c1fdec52d689308b767d09
Autarc <Autarc@gmail.com>
2013-05-27 14:03:21 +0200

    add .start()

--- src/meta/_game.js
+++ src/meta/_game.js
@@ -34,15 +34,23 @@ var Game = function ( id ) {
 
   //{ currentPlayers: 2, state: 'running' };
   // watchable: true // public, privated.., protected...
-  // this.config = { minPlayer: 2, maxPlayer: 8 };
+  this.config = { minPlayer: 2, maxPlayer: 8 };
 };
 
 
 utils.inherits( Game, Channel );
 
+
+/** start a game as minimal got reached **/
+Game.prototype.start = function ( init ) {
+
+  // just add players ? || not required - as done by data automaticly
+  if ( pg.data.length === this.config.minPlayer ) init();
+};
+
+
 // check for role | voting,as start -end etc. shouldnt be callable by the users afterwards....
 
-Game.prototype.start    = function(){};
 Game.prototype.end      = function(){};
 Game.prototype.pause    = function(){};
 Game.prototype.unpause  = function(){};
