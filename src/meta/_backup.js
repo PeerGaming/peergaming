02fbc18aeb074ac0149929b483ee97673994c0ce
Autarc <Autarc@gmail.com>
2013-07-19 18:19:52 +0200

    just remove 'player' from localStorage

--- src/meta/_backup.js
+++ src/meta/_backup.js
@@ -5,7 +5,7 @@
  *  Using local stored information to provide a backup for reconnection.
  */
 
-if ( LOCAL.player ) {
+if ( LOCAL['player'] ) {
 
   var range = parseFloat( JSON.parse(LOCAL.player).time ) +
               config.reconnectConfig.backupDuration - Date.now();
@@ -13,16 +13,18 @@ if ( LOCAL.player ) {
   if ( range >= 0 ) extend( BACKUP, JSON.parse(LOCAL.player) );
 }
 
-LOCAL.clear();
+delete LOCAL['player'];
 
 
 /**
  *  Overwrites the data in the localStorage
+ *
+ *  TODO: instead of always serializing the whole instance, just update the increment
  */
 
 function updateBackup() {
 
-  LOCAL.player = JSON.stringify(PLAYER);
+  LOCAL['player'] = JSON.stringify(PLAYER);
 }
 
 
